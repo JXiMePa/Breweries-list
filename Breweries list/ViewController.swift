532e43e94b7006de7ec50841e52db40c2a6ad1b3
Юрій Тарасенко <yurii.tarasenko@mova.io>
2020-10-22 19:32:19 +0300

    Initial Commit

---
+++ Breweries list/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Breweries list
+//
+//  Created by Tarasenko Jurik on 22.10.2020.
+//  Copyright © 2020 Tarasenko Yurii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
